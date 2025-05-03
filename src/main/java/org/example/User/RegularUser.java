976a2760f70e7dd867af6f9f180935ad7b0fc9c0
MkS391 <Mkrtich.s07@gmail.com>
2025-05-03 17:30:40 -0400

    a bit of work on regularUser

--- src/main/java/org/example/User/RegularUser.java
+++ src/main/java/org/example/User/RegularUser.java
@@ -3,17 +3,11 @@ package org.example.User;
 import org.example.Player.Player;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class RegularUser extends User {
     private ArrayList<Player> favoritePlayers;
-
-    public void viewSimpleStats(Player player){
-        //TODO
-    }
-    public void favoritePlayer(){
-        //TODO
-    }
 
     public RegularUser() {
         super();
@@ -22,6 +16,27 @@ public class RegularUser extends User {
     public RegularUser(ArrayList<Player> favoritePlayers) {
         super();
         this.favoritePlayers = favoritePlayers;
+    }
+    public void viewSimpleStats(Player player){
+        System.out.println(player.getName());
+        System.out.println("\nTeam: " + player.getTeam());
+        System.out.println("\nGoals: " + player.getGoals());
+        System.out.println("\nAssists: " + player.getAssists());
+    }
+    public void addFavoritePlayer(Player player) {
+        if (!favoritePlayers.contains(player)) {
+            favoritePlayers.add(player);
+            System.out.println(player.getName() + " has been added to favorites.");
+        } else {
+            System.out.println(player.getName() + " is already a favorite.");
+        }
+    }
+
+    public void viewFavoritePlayers() {
+        System.out.println("Favorite Players:");
+        for (Player p : favoritePlayers) {
+            System.out.println(p);
+        }
     }
 
     @Override
